b610a7df4ea71dd8500d16e5d7ce83a603db1788
Mauricio Paez <cornopaez@gmail.com>
2018-02-18 09:03:11 -0500

    Removed navbar from most views and deleted old files.

--- src/app/navbar/navbar.ts
+++
@@ -1,4 +0,0 @@
-export class NavBarItems {
-  title: string;
-  links: string[];
-}
---
+++ src/app/shared/view-content.service.ts
@@ -0,0 +1,104 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+// import { NavBarItems } from '../navbar/navbar'
+
+@Injectable()
+export class ViewContentService {
+
+  private ITEMS = [
+    {
+      route: 'Home',
+      content: {
+        // icons: array,
+        image: '../assets/close-up.png',
+        description: 'I\'m the description for this page!' ,
+        // source: string,
+        // footer: string
+        description_links: [
+          'Projects',
+          'About',
+          'Contact'
+        ]
+      }
+    },
+    {
+      route: 'Projects',
+      navbar: {
+        title: 'Projects',
+        links: [
+          'Home',
+          'About',
+          'Contact'
+        ]
+      },
+      title: 'Projects',
+      links: [
+        'Home',
+        'About',
+        'Contact'
+      ]
+    },
+    {
+      title: 'About',
+      links: [
+        'Home',
+        'Projects',
+        'Contact'
+      ]
+    },
+    {
+      title: 'Contact',
+      links: [
+        'Home',
+        'Projects',
+        'About'
+      ]
+    },
+    {
+      title: 'Portfolio',
+      links: [
+        'Home',
+        'Projects',
+        'About',
+        'Contact'
+      ],
+      icons: [
+        "devicon-html5-plain-wordmark",
+        "devicon-css3-plain-wordmark",
+        "devicon-sass-original",
+        "devicon-angularjs-plain",
+        "devicon-express-original-wordmark",
+        "devicon-nodejs-plain",
+        "devicon-heroku-plain"
+      ],
+      image: './assets/IMG_0307.jpg',
+      description: "This website is the first project I've taken on, at least on the Web Development front. It is designed with simplicity in mind following most of the concepts I have learned on Codecademy's HTML, CSS, SASS, and AngularJS tracks. Of course, this site is entirely over engineered to showcase some skills.",
+      source: 'https://github.com/cornopaez/cornopaez.github.io',
+      footer: 'Made by: Mauricio Páez - Pittsburgh 2017'
+    }
+  ]
+
+  // Observable NavBarItems source
+  private currentNavbarItemsSource = new Subject<any>();
+  private currentPageItemsSource = new Subject<any>();
+  // private currentNavbarItemsSource = new Subject<any>();
+
+  // Observable NavBarItems stream
+  currentNavbarItems$ = this.currentNavbarItemsSource.asObservable();
+  currentPageItem$ = this.currentPageItemsSource.asObservable();
+
+  // Set navbar contents
+  setPageContent(route: string) {
+
+    // Find items in array
+    function findItem(item) {
+      return item.route === route
+    }
+
+    // Define the data source
+    this.currentNavbarItemsSource.next(this.ITEMS.find(findItem))
+
+    // console.log('NavbarService - setNavbarItems: The route is "' + route + '" and the current navbar items are: ' + this.currentNavbarItems$)
+  }
+
+}
